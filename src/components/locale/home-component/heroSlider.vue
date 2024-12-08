c00861dc54dd018a758f2634f2f1782adbc7ee3c
naderkhaled15 <https://github.com/naderkhaled15>
2024-12-08 17:30:32 +0200

    hero content

--- src/components/locale/home-component/heroSlider.vue
+++ src/components/locale/home-component/heroSlider.vue
@@ -1,34 +1,25 @@
 <template>
-  <div>
-    <swiper
-      class="swiper"
-      style="height: 90vh"
-      :modules="modules"
-      :space-between="30"
-      :centered-slides="true"
-      :navigation="false"
-      :loop="true"
-      :effect="'fade'"
-      :autoplay="{
-        delay: 2000,
-        disableOnInteraction: false,
-      }"
-      :speed="500"
-    >
-      <swiper-slide v-for="(item, i) in 20" :key="i" class="slide">
-        <img
-          src="/src/assets/images/PHN_1434.webp"
-          style="
-            width: 100vw;
-            height: auto;
-            object-fit: contain;
-            object-position: center;
-          "
-          alt=""
-        />
-      </swiper-slide>
-    </swiper>
-  </div>
+  <swiper
+    class="swiper hero-swiper"
+    :modules="modules"
+    :space-between="30"
+    :centered-slides="true"
+    :navigation="false"
+    :loop="true"
+    :effect="'fade'"
+    :autoplay="{
+      delay: 2000,
+      disableOnInteraction: false,
+    }"
+    :speed="500"
+  >
+    <swiper-slide v-for="(item, i) in 20" :key="i" class="slide">
+      <img src="/src/assets/images/PHN_1434.webp" alt="" />
+      <div class="hero-content container grd grd-2-col">
+        <div>kj</div>
+      </div>
+    </swiper-slide>
+  </swiper>
 </template>
 
 <script setup>
