7a228854679934a14da907830e072e3f3e3bcea7
naderkhaled15 <https://github.com/naderkhaled15>
2024-12-08 17:55:28 +0200

    hero content

--- src/components/locale/home-component/heroSlider.vue
+++ src/components/locale/home-component/heroSlider.vue
@@ -16,7 +16,16 @@
     <swiper-slide v-for="(item, i) in 20" :key="i" class="slide">
       <img src="/src/assets/images/PHN_1434.webp" alt="" />
       <div class="hero-content container grd grd-2-col">
-        <div>kj</div>
+        <div class="w-100">
+          <p class="head-primary">خيارك الأول للتمثيل القانوني</p>
+          <p class="body-desc-primary">
+            بخبرة قانونية راسخة قدمنا الدعم للأفراد والشركات في المملكة لأكثر من
+            15 عاماً. نحن هنا لتقديم استشارات مدروسة وحلول قانونية مبتكرة لتحقيق
+            العدالة في كافة القضايا، مع الالتزام بأعلى المعايير الدولية.
+          </p>
+          <button class="btn-round-primary">احجز استشارتك القانونية</button>
+        </div>
+        <div class="bg-danger w-100"></div>
       </div>
     </swiper-slide>
   </swiper>
