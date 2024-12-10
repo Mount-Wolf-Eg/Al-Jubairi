88b8dba2cb0b38a10cd55871710c36f458e230e0
naderkhaled15 <https://github.com/naderkhaled15>
2024-12-10 17:46:21 +0200

    achieve-sec

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,10 +3,12 @@
     <heroSlider />
     <AboutUs />
     <Support />
+    <Achievements />
   </main>
 </template>
 <script setup>
 import heroSlider from "@/components/locale/home-component/heroSlider.vue";
 import AboutUs from "@/components/locale/home-component/AboutUs.vue";
 import Support from "@/components/locale/home-component/Support.vue";
+import Achievements from "@/components/locale/home-component/Achievements.vue";
 </script>
