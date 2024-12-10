92c8f951dcaf1f84491ef620ef6d154bcd687b85
naderkhaled15 <https://github.com/naderkhaled15>
2024-12-10 14:38:57 +0200

    feat(about us)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,10 @@
 <template>
-  <main><heroSlider /></main>
+  <main>
+    <heroSlider />
+    <AboutUs />
+  </main>
 </template>
 <script setup>
 import heroSlider from "@/components/locale/home-component/heroSlider.vue";
+import AboutUs from "@/components/locale/home-component/AboutUs.vue";
 </script>
