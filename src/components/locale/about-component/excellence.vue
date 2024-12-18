78436e3c87e2a7dc5078d4ba401f7ca2455afee7
naderkhaled15 <https://github.com/naderkhaled15>
2024-12-18 16:41:11 +0200

    fix(excellence)

--- src/components/locale/about-component/excellence.vue
+++ src/components/locale/about-component/excellence.vue
@@ -1,15 +1,15 @@
 <template>
   <div
-    class="legal-expertise d-flex align-items-center justify-content-center flex-column flex-md-row gap-5 gap-md-0 mx-auto"
+    class="legal-expertise d-flex align-items-center justify-content-center flex-column flex-md-row gap-3 gap-md-0 mx-auto"
   >
     <div
-      class="d-flex align-items-center align-items-md-start justify-content-center flex-column flex-md-row gap-3 gap-md-5"
+      class="d-flex align-items-center justify-content-center flex-column flex-md-row gap-3 gap-md-3"
       v-for="(item, i) in props.secData.items?.data?.slice(0, 3)"
       :key="i"
     >
       <img
         loading="lazy"
-        style="max-height: 6.4rem; max-width: 8.4rem; height: 100%; width: auto"
+        style="max-height: 6.4rem; max-width: 9rem; height: 100%; width: auto"
         :src="item.image?.media"
         :alt="item.image?.alt"
       />
