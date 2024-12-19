b2e40d64f4420c6e2acc0bdb2ff942dbda752525
naderkhaled15 <https://github.com/naderkhaled15>
2024-12-19 11:00:30 +0200

    add (excellence title)

--- src/components/locale/about-component/excellence.vue
+++ src/components/locale/about-component/excellence.vue
@@ -1,21 +1,26 @@
 <template>
-  <div
-    class="legal-expertise d-flex align-items-center justify-content-center flex-column flex-md-row gap-3 gap-md-0 mx-auto"
-  >
+  <div class="legal">
+    <p class="our-journey-sec--title head-secondary text-center">
+      {{ props.secData.title }}
+    </p>
     <div
-      class="d-flex align-items-center justify-content-center flex-column flex-md-row gap-3 gap-md-3"
-      v-for="(item, i) in props.secData.items?.data?.slice(0, 3)"
-      :key="i"
+      class="legal-expertise d-flex align-items-center justify-content-center flex-column flex-md-row gap-3 gap-md-0 mx-auto pt-5"
     >
-      <img
-        loading="lazy"
-        style="max-height: 6.4rem; max-width: 9rem; height: 100%; width: auto"
-        :src="item.image?.media"
-        :alt="item.image?.alt"
-      />
-      <p class="expertise-text">
-        {{ item.desc }}
-      </p>
+      <div
+        class="d-flex align-items-center justify-content-center flex-column flex-md-row gap-3 gap-md-3"
+        v-for="(item, i) in props.secData.items?.data?.slice(0, 3)"
+        :key="i"
+      >
+        <img
+          loading="lazy"
+          style="max-height: 6.4rem; max-width: 9rem; height: 100%; width: auto"
+          :src="item.image?.media"
+          :alt="item.image?.alt"
+        />
+        <p class="expertise-text">
+          {{ item.desc }}
+        </p>
+      </div>
     </div>
   </div>
 </template>
