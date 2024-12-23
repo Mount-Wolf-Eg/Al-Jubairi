67eee5c5bfe201170c67b76a4d46797f144cfc93
YasmineEl-shahat <yasmenelshahat754gmail.com>
2024-12-24 00:35:49 +0200

    emp photo

--- src/views/OurTeam.vue
+++ src/views/OurTeam.vue
@@ -18,7 +18,7 @@
           >
             <img
               loading="lazy"
-              :src="item?.image?.media ?? '/src/assets/icons/logo.svg'"
+              :src="item?.image?.media ?? logo"
               style="
                 width: 23rem;
                 height: 24.5rem;
@@ -48,6 +48,7 @@
 import { ref, onMounted } from "vue";
 import BreadCrump from "@/reusables/bread-crump/BreadCrump.vue";
 import SplashScreen from "@/components/locale/custom-components/SplashScreen.vue";
+import logo from "/src/assets/icons/logo.svg";
 
 const crump = ref([{ name: "menu.team", rout: "/our-team" }]);
 
