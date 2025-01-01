ac1186f96c46142477269fade75e9eaf52002787
DiaaMagdi <diaamagdi@hotmail.com>
2025-01-01 17:52:46 +0200

    google ads

--- src/components/GoogleAd.vue
+++ src/components/GoogleAd.vue
@@ -11,7 +11,7 @@ onMounted(() => {
   // Load the Google Analytics script
   const script = document.createElement("script");
   script.async = true;
-  script.src = "https://www.googletagmanager.com/gtag/js?id=G-4YTLWTH65K";
+  script.src = "https://www.googletagmanager.com/gtag/js?id=G-G-67FEM1N971";
   document.head.appendChild(script);
 
   // Initialize Google Analytics
@@ -21,7 +21,7 @@ onMounted(() => {
       window.dataLayer.push(arguments);
     }
     gtag("js", new Date());
-    gtag("config", "G-4YTLWTH65K");
+    gtag("config", "G-67FEM1N971");
   };
 });
 </script>
